7dcde6aeed100a2a518cd2aeef47b28a402ae3fc
Caetano Burjack <caetanoburjack@Caetanos-Laptop.local>
2023-01-20 13:34:28 +0000

    Updating Readme

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
+![Drag Racing](habits-web.png)
+
+
 # Habits
 
 Project developed during NLW Setup from RocketSeat.
 
 This project covers backend with nodeJs and Prisma, FrontEnd with ReactJS for web and ReactNative for mobile.
 
-## Some libs and techs used on the project
+### Some libs and techs used on the project
 
 * phosphor-react
 * tailwindcss
 * typescript
 * vite
-
-
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -36,7 +36,7 @@ export function SummaryTable() {
                 }
 
                 {
-                    amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                    amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_,i) => {
                         return (
                             <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
                         )
