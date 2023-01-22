071b6b8622aa36ac492b11d51c30dca3cfce0147
Caetano Burjack <caetano.burjack@gmail.com>
2023-01-22 20:36:00 +0000

    Adding some components.

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -30,13 +30,13 @@ export function SummaryTable() {
                 {
                     sumaryDates.map((date, i) => {
                         return (
-                            <HabitDay key={date.toString()} />
+                            <HabitDay amount={5} completed={Math.round(Math.random() * 5)} key={date.toString()} />
                         )
                     })
                 }
 
                 {
-                    amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_,i) => {
+                    amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                         return (
                             <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
                         )
